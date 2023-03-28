0075e94bff87f8bfa5c4c549a8a68b14416ecf71
Jorge Calvimontes <calvimontesjorges@gmail.com>
2023-03-28 10:37:29 +0200

    Read and write images

---
+++ dataset/pokemonDataset.py
@@ -0,0 +1,23 @@
+import os
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+
+class PokemonDataset (Dataset):
+    """
+    Custom map dataset that reads from folder path.
+    """
+    def __init__ (self, path='./pokemon', transform=None):
+        self.path = path
+        self.files_path = sorted (os.listdir (path))
+        self.transform = transform
+
+    def __len__ (self):
+        return len (self.files_path)
+
+    def __getitem__ (self, idx):
+        img_path = os.path.join (self.path, self.files_path [idx])
+        image = read_image (img_path)  # Read as RGBA chanels
+        if self.transform:
+            image = self.transform (image)
+        return image
---
+++ dataset/utils.py
@@ -0,0 +1,45 @@
+from torch import Tensor
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import numpy as np
+from .pokemonDataset import PokemonDataset
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use ('TkAgg')
+
+
+def prepare_data (batch_size=64):
+    """
+    Construct input pipeline using custom dataset and transformations
+    :param batch_size: size of each batch
+    :return: dataloader object for training
+    """
+    input_transform = transforms.Compose ([
+        transforms.RandomHorizontalFlip (),  # As stated on the paper
+        transforms.Lambda (lambda img: img / 255),  # In range [0,1]
+        transforms.Lambda (lambda img: (img * 2) - 1)  # In range [-1,1]
+    ])
+
+    dataset = PokemonDataset ('./pokemon', transform = input_transform)
+    train_dataloader = DataLoader (dataset, batch_size = 64, shuffle = True)
+    return train_dataloader
+
+
+np_transform = transforms.Compose ([
+    transforms.Lambda (lambda t: (t + 1) / 2),  # In range [0,1]
+    transforms.Lambda (lambda t: t.permute (1, 2, 0)),  # CHW to HWC
+    transforms.Lambda (lambda t: t * 255.),  # In range [0,255]
+    transforms.Lambda (lambda t: t.numpy ().astype (np.uint8)),
+])
+
+
+def output_to_image (t: Tensor):
+    array = np_transform (t)
+    return transforms.ToPILImage (array)
+
+
+def show_image (img: Tensor):
+    plt.imshow (np_transform (img))
+    plt.show ()
