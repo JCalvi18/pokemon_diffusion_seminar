136a6da02d08163e690eff12b2cb866b478ff385
Jorge Calvimontes <calvimontesjorges@gmail.com>
2023-04-06 20:06:52 +0200

    Notebook for final presentation

---
+++ notebook_utils.py
@@ -0,0 +1,22 @@
+# Class to simualate parser so to keep consistency
+class Args:
+    def __init__(self,
+        timesteps = None,
+        epochs = None,
+        batch = None,
+        dataset_path = './pokemon/',
+        load_path = None,
+        training_mode = False,
+        unet_version = 0,
+        seed = 132,
+        scale_down = False,
+    ) -> None:
+        self.timesteps = timesteps
+        self.epochs = epochs
+        self.batch = batch
+        self.dataset_path = dataset_path
+        self.load_path = load_path
+        self.training_mode = training_mode
+        self.unet_version = unet_version
+        self.seed = seed
+        self.scale_down = scale_down
